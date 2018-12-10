d512a628c1a64071e607cbbed00ea12d6e561c8f
Renan Santos Mendes <renansantosmendes@gmail.com>
2018-12-10 19:31:06 -0200

    Add files via upload

---
+++ 02 - Operadores.R
@@ -0,0 +1,48 @@
+## Operadores básicos, relacionais e logicos em R
+
+# Operadores básicos
+# Soma
+4 + 4
+
+# subtração
+4 - 4
+
+# multiplicação
+4 * 4
+
+# divisão
+4 / 4
+
+# potencia
+4^2
+4**2
+
+# módulo
+4 %% 2
+
+# Operadores relacionais
+# atribuindo variáveis
+a = 7
+b = 5  
+
+# operadores
+a > 8
+a < 8
+a <= 8
+a >= 8
+a == 8
+a != 8
+  
+# Operadores Lógicos
+# operador and
+(a == 8) & (b == 6)
+(a == 7) & (b == 5)
+(a == 8) & (b == 5)
+
+# operador or
+(a == 8) | (b > 5)
+
+# opeerador not
+a > 8
+print(! a > 8)
+
